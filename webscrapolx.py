5a19042d84dcf88309cdb9ce6a24f11796cd86cd
Mi-Sie <33319923+Mi-Sie@users.noreply.github.com>
2018-12-11 13:12:37 +0100

    Update webscrapolx.py

--- webscrapolx.py
+++ webscrapolx.py
@@ -2,12 +2,12 @@ from requests import get
 from bs4 import BeautifulSoup as bs
 
 ##import links
-subaruUrl = 'https://www.olx.pl/motoryzacja/samochody/subaru/forester/?search[filter_float_price:to]=40000&search[filter_float_year:from]=2002&search[filter_float_year:to]=2008&search[filter_float_enginepower:from]=200'
+dataUrl = 'https://www.olx.pl/motoryzacja/samochody/subaru/forester/?search[filter_float_price:to]=40000&search[filter_float_year:from]=2002&search[filter_float_year:to]=2008&search[filter_float_enginepower:from]=200'
 ## site: olx
 
 ## get the link
 
-urlContent = get(subaruUrl)
+urlContent = get(dataUrl)
 soup = BeautifulSoup.get()
 print(urlContent.content)
 
